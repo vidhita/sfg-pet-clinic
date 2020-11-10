0ce6fc42457112c4b193d84cec6a3c35a8ab7c63
vidhita <vidhita@outlook.com>
2020-11-10 12:56:56 +0530

    Files generated for Pet Clinic application

---
+++ src/main/java/java/spring/petclinic/SfgPetClinicApplication.java
@@ -0,0 +1,13 @@
+package java.spring.petclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfgPetClinicApplication.class, args);
+	}
+
+}
